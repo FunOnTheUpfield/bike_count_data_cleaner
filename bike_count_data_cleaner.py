7a44dc25e9b5dc32ce5bd13c9115b8cf5710a7ea
FunOnTheUpfield <bikenerd@gmail.com>
2016-05-22 13:28:38 +1000

    count summary page created

--- bike_count_data_cleaner.py
+++ bike_count_data_cleaner.py
@@ -17,8 +17,6 @@ from datetime import date,datetime,time
 EMPTY_VALUE = "NA"
 site_id 	= 0
 count_id 	= 0
-allsites 	= {}
-allcounts	= {}
 
 
 # Configuration -  Data Sources
@@ -46,6 +44,11 @@ siteout.write('\n')
 countout = open('../../Work/output/count_details.txt', 'w') 
 countout.write(' count_id | site_id | survey_year | survey_date | counting | bin_duration | gender_split | male_total | female_total | count_total')
 countout.write('\n')
+
+countsummaryout = open('../../Work/output/count_summary.txt', 'w')
+countsummaryout.write(' count_id | site_id | survey_date | counting  | count_total | from_north | from_east | from_south | from_west | to_north | to_east | to_south | to_west | male_total | male_from_north | male_from_east | male_from_south | male_from_west | male_to_north | male_to_east | male_to_south | male_to_west | female_total | female_from_north | female_from_east | female_from_south | female_from_west | female_to_north | female_to_east | female_to_south | female_to_west')
+countsummaryout.write('\n')
+
 
 moveout = open('../../Work/output/bike_movement_observations.txt', 'w')
 moveout.write('count_id | site_id | bin_start | bin_duration | gender | north_to_west | north_to_south | north_to_east | east_to_north | east_to_west | east_to_south | south_to_east | south_to_north | south_to_west | west_to_south | west_to_east | west_to_north')
@@ -82,11 +85,10 @@ for spreadsheet in excelfiles:
 				# This is true for super tue 7am-9am count but not for the upfield corridor count
 
 			sitedic['site_id']   = str(site_id)
-			allsites[str(site_id)] = sitedic
-
+			
 			# add results to site details file
 			create_output(sitedic,  siteout,  outputcols['SITECOLUMNS'])
-
+			
 			# Collect count details
 			for k in currentsheet['count_detail_row']:
 				count_details 		= currentsheet['count_detail_row'][k]['start_row']
@@ -103,6 +105,11 @@ for spreadsheet in excelfiles:
 					countdic['site_id'] = str(site_id)
 					count_id = count_id + 1
 					countdic['count_id'] = str(count_id)
+
+					countsummarydic = {}
+					countsummarydic['site_id'] = str(site_id)
+					countsummarydic['count_id'] = str(count_id)
+
 					for k in currentsheet['count_details_cells']:
 						row = currentsheet['count_details_cells'][k]['row'] + count_details
 						col = currentsheet['count_details_cells'][k]['col']
@@ -117,6 +124,7 @@ for spreadsheet in excelfiles:
 						survey_year = preformatted_date[0]                                                 
 						countdic['survey_date'] = formatted_date
 						countdic['survey_year'] = survey_year
+						countsummarydic['survey_date'] = formatted_date
 					except:
 						print 'Date problem'
 						# TODO: Handle this use case (doesn't come up in the super tue data sheet)
@@ -126,31 +134,37 @@ for spreadsheet in excelfiles:
 					# Where they are not specified in the supertue spreadsheet, look it up in 'spreadsheetdetails.json'
 					for k in currentsheet['count_detail_attributes']:                    
 						countdic[k] = currentsheet['count_detail_attributes'][k]['value']
+						countsummarydic[k] = currentsheet['count_detail_attributes'][k]['value']
 					try:
 						countdic['bin_duration'] = int(countdic['bin_duration'])
 					except:
 						continue
-					allcounts[str(count_id)] = countdic
-
 
 
 					# Collect the most detailed bike movement information stored in the spreadsheet, individual movements by time bin.
 
 					# Store general information about the count in the first columns of the row
 					movedic = {}
-					movedic['site_id']  		= countdic['site_id']
-					movedic['count_id'] 		= countdic['count_id']
+					movedic['site_id']  		= str(site_id)
+					movedic['count_id'] 		= str(count_id)
 					movedic['bin_duration'] 		= countdic['bin_duration'] 
 
 					counttotal = 0
+
 					# Check what information is stored in the supertue spread sheet 
-					# Some year's count information might only have 120 minute summaries (ie no 15 min breakdown)
-					# Other year's count information might not include gender breakdown. 
+					# Some year's count information might only have 120 minute summaries (ie no 15 min breakdown) and no gender details
+					# Other year's count information have 15min breakdowns but no gender breakdown. 
 
 					if countdic['bin_duration'] == 120:
-						print 'Old super tuesday count with no gendersplit, go to summary row'						
-						# TODO: Handle this use case
-					
+						gender = ('NA',)
+						# Skip to year's summary row in the spreadsheet for scrape
+						row = count_details + 27
+						col = currentsheet['oldcount_summary']['count_total']['col']						
+						counttotal = int(sheet.cell(row,col).value)
+						for oldie in currentsheet['oldcount_summary']:
+							col = currentsheet['oldcount_summary'][oldie]['col']
+							countsummarydic[oldie] =	int(sheet.cell(row,col).value)
+						
 					elif countdic['gender_split'] == 'N':
 						gender = ('NA',)
 					else:
@@ -190,19 +204,27 @@ for spreadsheet in excelfiles:
 							rowtotal = 0
 							
 							if 	movedic['gender'] != 'female': 
-								movement_lookup = 'male_movement_bin_columns'								
+								movement_lookup = 'male_total'
+								movements = ('male_total', 
+										'male_from_north','male_from_east', 'male_from_south', 'male_from_west',
+										'male_to_north', 'male_to_east', 'male_to_south', 'male_to_west')								
 								# When no gendersplit information was recorded, total male and female movements 
 								# were recorded in the male column.
 					
 							else:
-								movement_lookup = 'female_movement_bin_columns'
-							
+								movement_lookup = 'female_total'
+								movements = ('female_total', 
+										'female_from_north','female_from_east', 'female_from_south', 'female_from_west',
+										'female_to_north', 'female_to_east', 'female_to_south', 'female_to_west')								
+
+
+							# Collect most detailed observations availabel
 							for i in currentsheet[movement_lookup]:
 								col = currentsheet[movement_lookup][i]['col']
 								move = sheet.cell(row,col).value
+								# Sum rows where possible.
 								try:
-									move = int(move)
-									rowtotal = rowtotal + move
+									rowtotal = rowtotal + int(move)
 								except:
 									continue
 
@@ -211,17 +233,68 @@ for spreadsheet in excelfiles:
 							# Output the row of observations, and the row summaries to text file
 							create_output(movedic, moveout, outputcols['MOVECOLUMNS'])
 
+							for summaries in (movements):
+								summarytotal = 0
+								for i in currentsheet[summaries]:									
+									col = currentsheet[summaries][i]['col']
+									turn = sheet.cell(row,col).value
+									try:
+										summarytotal = summarytotal + int(turn)
+									except:
+										continue
+								countsummarydic[summaries] = summarytotal
+
 							#Sum scraped values to create count totals
 							genderedtotal = genderedtotal + rowtotal			
+						counttotal = counttotal + genderedtotal
+						
 						if mf == 'male':
 							countdic['male_total'] = genderedtotal
 						elif mf == 'female':
 							countdic['female_total'] = genderedtotal
 						else:
-							print movedic
-						counttotal = counttotal + genderedtotal
-					countdic['count_total'] = counttotal	
+							countdic['male_total'] = 'NA'
+							countdic['female_total'] = 'NA'
+
+							# Counts with no gender split should have 'NA' rather than '0' in the gender breakdown fields					
+							countsummarydic['male_total'] = 'NA'
+							countsummarydic['male_from_north'] = 'NA'
+							countsummarydic['male_from_east'] = 'NA'
+							countsummarydic['male_from_south'] = 'NA'
+							countsummarydic['male_from_west'] = 'NA'
+							countsummarydic['male_to_north'] = 'NA'
+							countsummarydic['male_to_east'] = 'NA'
+							countsummarydic['male_to_south'] = 'NA'
+							countsummarydic['male_to_west'] = 'NA'
+							countsummarydic['female_total'] = 'NA'
+							countsummarydic['female_from_north'] = 'NA'
+							countsummarydic['female_from_east'] = 'NA'
+							countsummarydic['female_from_south'] = 'NA'
+							countsummarydic['female_from_west'] = 'NA'
+							countsummarydic['female_to_north'] = 'NA'
+							countsummarydic['female_to_east'] = 'NA'
+							countsummarydic['female_to_south'] = 'NA'
+							countsummarydic['female_to_west'] = 'NA'
+
+						# Add the male and female summaries together - where numeric data is available
+						try:
+							countsummarydic['from_north']	= int(countsummarydic['male_from_north']+ countsummarydic['female_from_north'])
+							countsummarydic['from_east'] 	= int(countsummarydic['male_from_east'] + countsummarydic['female_from_east'])
+							countsummarydic['from_south'] = int(countsummarydic['male_from_south'] + countsummarydic['female_from_south'])
+							countsummarydic['from_west'] 	= int(countsummarydic['male_from_west'] + countsummarydic['female_from_west'])
+							countsummarydic['to_north'] 	= int(countsummarydic['male_to_north'] + countsummarydic['female_to_north'])
+							countsummarydic['to_east'] 	= int(countsummarydic['male_to_east'] + countsummarydic['female_to_east'])
+							countsummarydic['to_south'] 	= int(countsummarydic['male_to_south'] + countsummarydic['female_to_south'])
+							countsummarydic['to_west'] 	= int(countsummarydic['male_to_west'] + countsummarydic['female_to_west'])
+
+						except:
+							continue
+											
+					countdic['count_total'] = counttotal
+					countsummarydic['count_total'] = counttotal
+	
 
 					# add results to count details file
 					create_output(countdic,  countout,  outputcols['COUNTCOLUMNS'])
+					create_output(countsummarydic, countsummaryout, outputcols['COUNTSUMMARYOUT'])
 
